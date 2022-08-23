571597d2b8b674fd8fac94130e9ffaf0b42d56b6
yousef-essa <yousef.essa.p@gmail.com>
2022-08-23 18:59:48 +0200

    Add download progress bar

--- app/src/main/java/io/yousefessa/robotimer/update/adapter/UpdaterAdapter.java
+++ app/src/main/java/io/yousefessa/robotimer/update/adapter/UpdaterAdapter.java
@@ -6,31 +6,64 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.util.UUID;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.FileProvider;
 import io.yousefessa.applicationupdater.adapter.ApplicationAdapter;
+import io.yousefessa.applicationupdater.adapter.ApplicationAdapterContext;
 import io.yousefessa.robotimer.AndroidApplication;
+import io.yousefessa.robotimer.R;
 import org.jetbrains.annotations.NotNull;
 
 public class UpdaterAdapter implements ApplicationAdapter {
 
-    public static final String APPLICATION_VND_ANDROID_PACKAGE_ARCHIVE = "application/vnd.android.package-archive";
-    public static final int DEFAULT_BUFFER_SIZE = 8 * 1024;
+    private static final String APPLICATION_VND_ANDROID_PACKAGE_ARCHIVE = "application/vnd.android.package-archive";
+    private static final int DEFAULT_BUFFER_SIZE = 8 * 1024;
+    private static final String NOTIFICATION_CHANNEL_ID = "downloading_an_update";
+    private static final int NOTIFICATION_ID = 100;
+    public static final String NOTIFICATION_TITLE = "Downloading an update";
+
+    private final NotificationCompat.Builder notificationBuilder;
+    private final NotificationManagerCompat notificationManager;
+
+    public UpdaterAdapter() {
+        final AndroidApplication context = AndroidApplication.getInstance();
+
+        this.notificationManager = NotificationManagerCompat.from(context);
+
+        this.notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(NOTIFICATION_TITLE)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.ic_launcher);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            final NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_TITLE,
+                    NotificationManager.IMPORTANCE_LOW);
+            this.notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
 
     @SuppressLint("SetWorldReadable")
     @Override
-    public void onDownload(@NotNull final InputStream input) {
+    public void onDownload(@NotNull final ApplicationAdapterContext context) {
+        final AndroidApplication applicationContext = AndroidApplication.getInstance();
+        final InputStream input = context.getInputStream();
+        final HttpURLConnection connection = (HttpURLConnection) context.getConnection();
         try {
-            final AndroidApplication applicationContext = AndroidApplication.getInstance();
 
-            final File apkFile = downloadFile(applicationContext, input);
+            final File apkFile = downloadFile(applicationContext, input, connection);
             if (apkFile == null) {
                 System.out.println("The downloading process has failed.");
                 return;
@@ -47,8 +80,7 @@ public class UpdaterAdapter implements ApplicationAdapter {
         final Intent installPackageIntent;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            final Uri apkUri = FileProvider.getUriForFile(context,
-                    context.getPackageName() + ".provider", apkFile);
+            final Uri apkUri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", apkFile);
             installPackageIntent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
             installPackageIntent.setData(apkUri);
             installPackageIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -72,15 +104,15 @@ public class UpdaterAdapter implements ApplicationAdapter {
         //  package, that way we would be able to delete
         //  the apk file once the installation process
         //  is complete
-//        if (apkFile.delete()) {
-//            System.out.println("The apk file has been deleted successfully.");
-//        } else {
-//            System.out.println("The apk file has been deleted unsuccessfully.");
-//        }
+        //        if (apkFile.delete()) {
+        //            System.out.println("The apk file has been deleted successfully.");
+        //        } else {
+        //            System.out.println("The apk file has been deleted unsuccessfully.");
+        //        }
     }
 
     @SuppressLint("SetWorldReadable")
-    private File downloadFile(final Context context, final InputStream input) {
+    private File downloadFile(final Context context, final InputStream input, final HttpURLConnection connection) {
         System.out.println("Starting downloading process now!");
         final File downloadDirectory = new File(context.getExternalFilesDir(null), "update");
 
@@ -103,8 +135,7 @@ public class UpdaterAdapter implements ApplicationAdapter {
             }
         }
 
-        // todo: unsupported due to the connection not being exposed here... fix this!
-        final long fileLength = -1;
+        final long fileLength = connection.getContentLength();
 
         final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
         int bytes;
@@ -120,12 +151,25 @@ public class UpdaterAdapter implements ApplicationAdapter {
             return null;
         } finally {
             apkFile.setReadable(true, false);
+            updateProgress(100);
         }
         return apkFile;
     }
 
     private void updateProgress(final int progress) {
         System.out.println("progress: " + progress);
-        // todo: implement this after fileLength variable is ready to be used
+
+        if (progress == 100) {
+            notificationManager.cancel(NOTIFICATION_ID);
+            return;
+        }
+
+        notificationBuilder
+                .setOngoing(true)
+                .setProgress(100, progress, false)
+                .setContentInfo(progress + "%");
+
+        final Notification notification = notificationBuilder.build();
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
