f41cfec535c50db98378c29ce6ba82a0e8656104
yousef-essa <yousef.essa.p@gmail.com>
2022-08-23 02:20:37 +0200

    Improve update thread-safety & practice

--- app/src/main/java/io/yousefessa/robotimer/update/adapter/UpdateController.java
+++ app/src/main/java/io/yousefessa/robotimer/update/adapter/UpdateController.java
@@ -2,48 +2,21 @@ package io.yousefessa.robotimer.update.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import io.yousefessa.robotimer.util.ApplicationMainLooper;
 
-public class UpdateController {
-    private Intent installIntent;
+public interface UpdateController {
+    void requestForPackageInstall(final Context context, final Intent packageIntent);
 
-    public void requestForPackageInstall(final Context context, final Intent installIntent) {
-        installIntent(installIntent);
+    void launchUpdateActivity(final Context context);
+    void launchUpdateActivity(final Context context, final Intent packageIntent);
 
-        ApplicationMainLooper.instance()
-                .post(() -> {
-                    final Intent updateIntent = new Intent(context, UnknownAppSourceRequestActivity.class);
-                    updateIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(updateIntent);
-                    installIntent(null);
-                });
+    void packageIntent(final Intent intent);
+    Intent packageIntent();
+
+    class SingletonHolder {
+        private static final DefaultUpdateController INSTANCE = new DefaultUpdateController();
     }
 
-    public void launchUpdateActivity(final Context context, final Intent installPackageIntent) {
-        if (installPackageIntent == null) {
-            return;
-        }
-
-        ApplicationMainLooper.instance()
-                .post(() -> {
-                    context.startActivity(installPackageIntent);
-                    installIntent(null);
-                });
-    }
-
-    public void launchUpdateActivity(final Context context) {
-        launchUpdateActivity(context, installIntent());
-    }
-
-    public void installIntent(final Intent inputStream) {
-        this.installIntent = inputStream;
-    }
-
-    public Intent installIntent() {
-        return installIntent;
-    }
-
-    public static class Singleton {
-        public static final UpdateController INSTANCE = new UpdateController();
+    static UpdateController getInstance() {
+        return SingletonHolder.INSTANCE;
     }
 }
