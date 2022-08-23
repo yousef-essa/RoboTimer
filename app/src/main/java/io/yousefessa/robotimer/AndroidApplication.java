defc682f3b8efd2ae15f7b06d1c676ca94922d99
yousef-essa <yousef.essa.p@gmail.com>
2022-08-23 21:33:28 +0200

    Fix version scheme including -release

--- app/src/main/java/io/yousefessa/robotimer/AndroidApplication.java
+++ app/src/main/java/io/yousefessa/robotimer/AndroidApplication.java
@@ -4,7 +4,8 @@ import android.app.Application;
 
 public class AndroidApplication extends Application {
     private static AndroidApplication instance;
-    private static final String LOCAL_VERSION = "0.1.0-" + BuildConfig.BUILD_TYPE;
+    private static final String LOCAL_VERSION = "0.1.0" + (BuildConfig.BUILD_TYPE.equals("release") ? "" :
+            "-" + BuildConfig.BUILD_TYPE);
 
     public static AndroidApplication getInstance() {
         return instance;
